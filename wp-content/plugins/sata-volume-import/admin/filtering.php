e95c1d52283d024f03804d8b109022f764da8163
Sean Dove <sdove@schlagergroup.com>
2017-06-01 15:20:31 -0500

    created sata import plugin

---
+++ wp-content/plugins/sata-volume-import/admin/filtering.php
@@ -0,0 +1,10 @@
+<?php
+
+    namespace sata_import_namespace;
+
+    if($xml){ 
+        echo '<h3>Filtering</h3>';
+        echo '<h4>Only in <a href="http://zefirstudio.pl/wp-best-import/">Best Import Pro</a></h4>';    
+    }
+
+?>
